314531397a64d93ebd3c2e0b5bcb7da7042057a8
Andreas Grøntved <andreasgro@gmail.com>
2019-03-05 15:31:19 +0100

    Virker måske...

--- IoTAPI/src/main/java/IoTAPI/Start.kt
+++ IoTAPI/src/main/java/IoTAPI/Start.kt
@@ -1,6 +1,18 @@
 package IoTAPI
 
+import AuthManager.Hest5
+import DeviceManager.Hest3
+import IoTDevices.Hest4
+import Tangle.Hest6
+
 
 fun main(args: Array<String>) {
     println("Hello, world!")
+
+    val h = Hest6()
+    val i =Hest4()
+    val j = Hest2()
+    val k = Hest3()
+    val l = Hest5()
+
 }
