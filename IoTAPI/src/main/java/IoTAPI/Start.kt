134f45ddb9d8831b6d944c033105bdad51e1278c
MegaCakeEater <Emil_ngaard@hotmail.com>
2019-03-06 13:28:18 +0100

    Heat pump prototype and simple client

--- IoTAPI/src/main/java/IoTAPI/Start.kt
+++ IoTAPI/src/main/java/IoTAPI/Start.kt
@@ -2,7 +2,7 @@ package IoTAPI
 
 import AuthManager.Hest5
 import DeviceManager.Hest3
-import IoTDevices.Hest4
+import IoTDevices.HeatPump.HeatPump
 import Tangle.Hest6
 
 
@@ -10,7 +10,7 @@ fun main(args: Array<String>) {
     println("Hello, world!")
 
     val h = Hest6()
-    val i =Hest4()
+    val i = HeatPump()
     val j = Hest2()
     val k = Hest3()
     val l = Hest5()
